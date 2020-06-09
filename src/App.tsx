f850f324b3e3844ec109e8cfe57b526bc8ccfbce
wildlifehexagon <crossthebreeze@gmail.com>
2020-06-09 15:31:35 -0500

    feat: add FixedRow component and initial tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Grid from "@material-ui/core/Grid"
+import { makeStyles } from "@material-ui/core/styles"
+import FixedRow from "./FixedRow"
 
-function App() {
+const useStyles = makeStyles({
+  layout: {
+    width: "85%",
+    margin: "auto",
+    paddingTop: "100px",
+  },
+})
+
+const App = () => {
+  const classes = useStyles()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Grid container className={classes.layout}>
+      <Grid item xs={12}>
+        <FixedRow />
+      </Grid>
+    </Grid>
+  )
 }
 
-export default App;
+export default App
---
+++ src/FixedRow.tsx
@@ -0,0 +1,64 @@
+import React from "react"
+import { useVirtual } from "react-virtual"
+import Paper from "@material-ui/core/Paper"
+import List from "@material-ui/core/List"
+import ListItem from "@material-ui/core/ListItem"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles(() => ({
+  container: {
+    height: "200px",
+    overflow: "auto",
+  },
+  row: {
+    borderBottom: "1px solid rgba(224, 224, 224, 1)",
+    "&:hover": {
+      backgroundColor: "#eeeeee",
+      boxShadow:
+        "inset 1px 0 0 #dadce0,inset -1px 0 0 #dadce0,0 1px 2px 0 rgba(60,64,67,.3),0 1px 3px 1px rgba(60,64,67,.15)",
+      zIndex: 1,
+    },
+  },
+}))
+
+const FixedRow = () => {
+  const parentRef = React.useRef<HTMLDivElement>(null)
+  const rowVirtualizer = useVirtual({
+    size: 10000,
+    parentRef,
+    estimateSize: React.useCallback(() => 35, []),
+    overscan: 5,
+  })
+  const classes = useStyles(rowVirtualizer.totalSize)
+
+  return (
+    <Paper ref={parentRef} className={classes.container}>
+      <List>
+        {rowVirtualizer.virtualItems.map((virtualRow) => {
+          console.log(virtualRow)
+          return (
+            <ListItem
+              key={virtualRow.index}
+              className={classes.row}
+              id={`row-${virtualRow.index}`}
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                // size is 35px in this example
+                height: `${virtualRow.size}px`,
+                // repositions element vertically on 2D plane
+                // virtualRow.start is incremented by 35 each time here
+                transform: `translateY(${virtualRow.start}px)`,
+              }}>
+              Row {virtualRow.index}
+            </ListItem>
+          )
+        })}
+      </List>
+    </Paper>
+  )
+}
+
+export default FixedRow
