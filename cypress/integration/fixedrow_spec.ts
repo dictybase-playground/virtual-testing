abefe3766f117c1ff2e286dab6403e1a6f090b7e
wildlifehexagon <crossthebreeze@gmail.com>
2020-06-10 16:25:32 -0500

    test: fix broken cypress test

--- cypress/integration/fixedrow_spec.ts
+++ cypress/integration/fixedrow_spec.ts
@@ -3,9 +3,9 @@
 describe("FixedRow", () => {
   it("shows more rows on scroll", () => {
     cy.visit("/")
-    cy.contains("Row 0")
+    cy.contains("Row 1")
     cy.get("#parent-ref").scrollTo(0, 500)
     cy.contains("Row 10")
-    cy.get("#row-11").should("not.exist")
+    cy.get("#row-12").should("not.exist")
   })
 })
