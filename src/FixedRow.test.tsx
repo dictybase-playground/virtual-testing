82901554601d1c6fb32e578341f55bff97cd1399
wildlifehexagon <crossthebreeze@gmail.com>
2020-06-10 08:30:22 -0500

    test: mock scrolling on parent ref

--- src/FixedRow.test.tsx
+++ src/FixedRow.test.tsx
@@ -6,9 +6,12 @@ import List from "@material-ui/core/List"
 import ListItem from "@material-ui/core/ListItem"
 
 describe("FixedRow", () => {
-  jest
-    .spyOn(React, "useRef")
-    .mockReturnValueOnce({ current: document.createElement("div") })
+  const spyRef = jest.spyOn(React, "useRef").mockReturnValueOnce({
+    current: {
+      scrollTo: jest.fn(),
+      scroll: jest.fn(),
+    },
+  })
   const wrapper = shallow(<FixedRow />)
   it("should render outer components", () => {
     expect(wrapper.find(Paper)).toHaveLength(1)
@@ -31,11 +34,10 @@ describe("FixedRow", () => {
   it("should not render seventh row", () => {
     expect(wrapper.find("#row-6")).toHaveLength(0)
   })
-  it("should update rows on keydown", () => {
-    wrapper
-      .find("#row-5")
-      .simulate("keyDown", { key: "ArrowDown", keyCode: 40, which: 40 })
+  it("should track scrolling on parent ref", () => {
+    wrapper.find(Paper).simulate("scroll")
     wrapper.update()
     console.log(wrapper.debug())
+    expect(spyRef).toBeCalled()
   })
 })
