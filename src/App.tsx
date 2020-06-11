2984ac87583259045399025db99e58c8321606b3
wildlifehexagon <crossthebreeze@gmail.com>
2020-06-10 21:03:11 -0500

    feat: add graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import React from "react"
+import { ApolloProvider } from "@apollo/react-hooks"
+import { ApolloClient } from "apollo-client"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { createHttpLink } from "apollo-link-http"
+import { setContext } from "apollo-link-context"
 import Grid from "@material-ui/core/Grid"
 import { makeStyles } from "@material-ui/core/styles"
-import FixedRow from "../src/FixedRow"
+import FixedRow from "./FixedRow"
+import GraphQLContainer from "./GraphQLContainer"
+
+const client = new ApolloClient({
+  link: setContext((request, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        "X-GraphQL-Method": "Query",
+      },
+    }
+  }).concat(
+    createHttpLink({
+      uri: "https://ericgraphql.dictybase.dev/graphql",
+      credentials: "include",
+    }),
+  ),
+  cache: new InMemoryCache(),
+})
 
 const useStyles = makeStyles({
   layout: {
@@ -15,11 +38,16 @@ const App = () => {
   const classes = useStyles()
 
   return (
-    <Grid container className={classes.layout}>
-      <Grid item xs={12}>
-        <FixedRow />
+    <ApolloProvider client={client}>
+      <Grid container className={classes.layout}>
+        <Grid item xs={12}>
+          <GraphQLContainer />
+        </Grid>
+        <Grid item xs={12}>
+          <FixedRow />
+        </Grid>
       </Grid>
-    </Grid>
+    </ApolloProvider>
   )
 }
 
