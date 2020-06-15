74e76bd2918ca185c7b786373b606737881716a3
wildlifehexagon <crossthebreeze@gmail.com>
2020-06-15 09:06:23 -0500

    test: update e2e test with strain data

--- cypress/integration/infinitelist_spec.ts
+++ cypress/integration/infinitelist_spec.ts
@@ -3,9 +3,9 @@
 describe("InfiniteList", () => {
   it("shows more rows on scroll", () => {
     cy.visit("/")
-    cy.contains("cytB-")
+    cy.contains("DBS0351367")
     cy.get("#parent-ref").scrollTo(0, 500)
-    cy.contains("gnt15-")
+    cy.contains("DBS0351356")
     cy.get("#row-20").should("not.exist")
   })
 })
