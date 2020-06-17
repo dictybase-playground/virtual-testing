c5f136ae43a1814cb4ee8387171edaee0587c7c0
wildlifehexagon <crossthebreeze@gmail.com>
2020-06-17 10:17:40 -0500

    test: add e2e test for phenotype list

---
+++ cypress/integration/IntersectingPhenotypeList_spec.ts
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe("IntersectingPhenotypeList", () => {
+  it("shows more rows on scroll", () => {
+    cy.visit("/")
+    cy.contains("Use Intersecting (Phenotype)")
+    cy.get('a[href*="use-intersecting-phenotype"]').click()
+    cy.contains("DBS0350576")
+    cy.get("#parent-ref").scrollTo(0, 500)
+    cy.contains("DBS0349879")
+    cy.contains("DBS0350580")
+    cy.get("#parent-ref").scrollTo(0, 1000)
+    cy.get("#row-15").should("not.exist")
+  })
+})
