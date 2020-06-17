87adf39971160c8824b6a01e2e5f4ecf2327d5e6
wildlifehexagon <crossthebreeze@gmail.com>
2020-06-16 20:39:11 -0500

    test: add test for error handling graphql

--- src/IntersectingListContainer.test.tsx
+++ src/IntersectingListContainer.test.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { mount } from "enzyme"
 import { MockedProvider } from "@apollo/react-testing"
+import { act } from "react-dom/test-utils"
 import IntersectingListContainer from "./IntersectingListContainer"
 import IntersectingList from "./IntersectingList"
 import { GET_STRAIN_LIST } from "./query"
@@ -16,42 +17,88 @@ describe("IntersectingListContainer", () => {
       unobserve: jest.fn(),
     }))
   })
-  const mocks = [
-    {
-      request: {
-        query: GET_STRAIN_LIST,
-        variables: {
-          cursor: 0,
-          limit: 10,
-          filter: "",
+  describe("loading and inital data display", () => {
+    const mocks = [
+      {
+        request: {
+          query: GET_STRAIN_LIST,
+          variables: {
+            cursor: 0,
+            limit: 10,
+            filter: "",
+          },
         },
-      },
-      result: {
-        data: {
-          listStrains: {
-            nextCursor: 123456,
-            strains: [
-              {
-                id: "DBS0238532",
-                label: "γS13",
-              },
-            ],
+        result: {
+          data: {
+            listStrains: {
+              nextCursor: 123456,
+              strains: [
+                {
+                  id: "DBS0238532",
+                  label: "γS13",
+                },
+              ],
+            },
           },
         },
       },
-    },
-  ]
-  const wrapper = mount(
-    <MockedProvider mocks={mocks} addTypename={false}>
-      <IntersectingListContainer />
-    </MockedProvider>,
-  )
-  it("renders loading message first", () => {
-    expect(wrapper.find("p").text()).toContain("Loading...")
+    ]
+    const wrapper = mount(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <IntersectingListContainer />
+      </MockedProvider>,
+    )
+    it("renders loading message first", () => {
+      expect(wrapper.find("p").text()).toContain("Loading...")
+    })
+    it("renders expected components after receiving data", async () => {
+      await act(async () => {
+        await wait()
+        wrapper.update()
+      })
+      expect(wrapper.find(IntersectingList)).toHaveLength(1)
+    })
   })
-  it("renders expected components after receiving data", async () => {
-    await wait()
-    wrapper.update()
-    expect(wrapper.find(IntersectingList)).toHaveLength(1)
+
+  describe("error handling", () => {
+    const mocks = [
+      {
+        request: {
+          query: GET_STRAIN_LIST,
+          variables: {
+            cursor: 0,
+            limit: 10,
+            filter: "",
+          },
+        },
+        result: {
+          errors: [
+            {
+              message: "Strains not found",
+              path: [],
+              extensions: { code: "NotFound" },
+              locations: undefined,
+              nodes: undefined,
+              source: undefined,
+              positions: undefined,
+              originalError: undefined,
+              name: "",
+            },
+          ],
+        },
+      },
+    ]
+    const wrapper = mount(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <IntersectingListContainer />
+      </MockedProvider>,
+    )
+    it("handles errors as expected", async () => {
+      await act(async () => {
+        await wait()
+        wrapper.update()
+      })
+      expect(wrapper.find("p").text()).toContain("Error :(")
+    })
   })
 })
