c4e9e03e36de2ab9c662df0d9da57101533a9fad
wildlifehexagon <crossthebreeze@gmail.com>
2020-06-17 09:09:51 -0500

    test: revise cypress test to click link

--- cypress/integration/infinitelist_spec.ts
+++ cypress/integration/infinitelist_spec.ts
@@ -3,9 +3,11 @@
 describe("InfiniteList", () => {
   it("shows more rows on scroll", () => {
     cy.visit("/")
-    // cy.contains("DBS0351367")
-    // cy.get("#parent-ref").scrollTo(0, 500)
-    // cy.contains("DBS0351356")
-    // cy.get("#row-20").should("not.exist")
+    cy.contains("Use Intersecting")
+    cy.get('a[href*="use-intersecting"]').click()
+    cy.contains("DBS0351367")
+    cy.get("#parent-ref").scrollTo(0, 500)
+    cy.contains("DBS0351356")
+    cy.get("#row-20").should("not.exist")
   })
 })
