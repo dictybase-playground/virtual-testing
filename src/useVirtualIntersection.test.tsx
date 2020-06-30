ce4e5df60855493138ae392d029729be45ca98e0
wildlifehexagon <crossthebreeze@gmail.com>
2020-06-30 16:36:22 -0500

    feat: copy useVirtual logic into new combined hook

---
+++ src/useVirtualIntersection.test.tsx
@@ -0,0 +1,51 @@
+import { renderHook } from "@testing-library/react-hooks"
+import useVirtualIntersection from "./useVirtualIntersection"
+
+describe("useVirtualIntersection", () => {
+  let ref = null as any
+
+  beforeAll(() => {
+    ref = { current: null }
+  })
+
+  describe("items returned", () => {
+    const { result } = renderHook(() =>
+      useVirtualIntersection({
+        ref,
+        rowHeight: 30,
+        numItems: 100,
+        viewportHeight: 300,
+      }),
+    )
+    const items = result.current.items
+    it("should return correct amount of items", () => {
+      expect(items.length).toBe(11) // (300 / 30) + 1
+    })
+    it("should have correctly calculated transform values", () => {
+      expect(items[0].style.transform).toBe("translateY(0px)")
+      expect(items[10].style.transform).toBe("translateY(300px)")
+    })
+  })
+
+  describe("items returned with overscan", () => {
+    const { result } = renderHook(() =>
+      useVirtualIntersection({
+        ref,
+        rowHeight: 30,
+        numItems: 100,
+        viewportHeight: 300,
+        overscan: 2,
+      }),
+    )
+    const items = result.current.items
+    it("should return correct amount of items", () => {
+      // same as previous test but with two rows below
+      expect(items.length).toBe(13)
+    })
+    it("should have correctly calculated transform values", () => {
+      expect(items[0].style.transform).toBe("translateY(0px)")
+      expect(items[10].style.transform).toBe("translateY(300px)")
+      expect(items[12].style.transform).toBe("translateY(360px)")
+    })
+  })
+})
---
+++ src/useVirtualIntersection.tsx
@@ -0,0 +1,76 @@
+import React from "react"
+
+type ConfigParams = {
+  /** React ref used to access DOM node */
+  ref: React.MutableRefObject<any>
+  /** Height of the scrollable area (inner container) */
+  viewportHeight: number
+  /** Height of individual row */
+  rowHeight: number
+  /** Number of items in total */
+  numItems: number
+  /** Number of elements to render above and below viewport */
+  overscan?: number
+}
+
+const useVirtualIntersection = ({
+  ref,
+  viewportHeight,
+  rowHeight,
+  numItems,
+  overscan,
+}: ConfigParams) => {
+  /**
+   * scrollTop measures how far the inner container is scrolled.
+   * It is the difference between the total list height and the viewport
+   * height.
+   */
+  const [scrollTop, setScrollTop] = React.useState(0)
+
+  let startIndex = Math.floor(scrollTop / rowHeight)
+  let endIndex = Math.min(
+    numItems - 1, // don't render past the end of the list
+    Math.floor((scrollTop + viewportHeight) / rowHeight),
+  )
+
+  if (overscan) {
+    // take zero or the index minus overscan, whichever is higher
+    startIndex = Math.max(0, startIndex - overscan)
+    // if end of list, don't display the extra elements
+    endIndex = Math.min(numItems - 1, endIndex + overscan)
+  }
+
+  const items = []
+  for (let i = startIndex; i <= endIndex; i++) {
+    items.push({
+      // index is required so we know the exact row of data to display
+      index: i,
+      style: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: `${rowHeight}px`,
+        transform: `translateY(${i * rowHeight}px)`,
+      },
+    })
+  }
+
+  const handleScroll = (event: React.UIEvent<HTMLElement>) => {
+    setScrollTop(event.currentTarget.scrollTop)
+  }
+
+  React.useEffect(() => {
+    if (ref && ref.current) {
+      const element = ref.current
+      element.addEventListener("scroll", handleScroll)
+
+      return () => element.removeEventListener("scroll", handleScroll)
+    }
+    return
+  }, [ref])
+
+  return { items }
+}
+
+export default useVirtualIntersection
