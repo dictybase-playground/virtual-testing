de299f7528bc32558ab77e7e1e42fa48482f0b76
wildlifehexagon <crossthebreeze@gmail.com>
2020-07-01 10:50:03 -0500

    test: fix broken tests for useIntersecting

--- src/useIntersecting.test.tsx
+++ src/useIntersecting.test.tsx
@@ -1,14 +1,15 @@
+import React from "react"
 import { renderHook } from "@testing-library/react-hooks"
 import useIntersecting from "./useIntersecting"
 
 describe("useIntersecting", () => {
-  let ref = null as any
+  let targetRef = null as any
   const window = global as any
   let mockObserve: jest.Mock
   let mockUnobserve: jest.Mock
 
   beforeAll(() => {
-    ref = { current: null }
+    targetRef = { current: <div /> }
   })
 
   describe("observe and unobserve methods", () => {
@@ -25,15 +26,15 @@ describe("useIntersecting", () => {
     afterEach(() => {
       jest.clearAllMocks()
     })
-    it("should call observe when ref is passed", () => {
-      renderHook(() => useIntersecting({ ref, hasMore: true }))
+    it("should call observe when targetRef is passed", () => {
+      renderHook(() => useIntersecting({ targetRef, hasMore: true }))
       expect(mockObserve).toHaveBeenCalledTimes(1)
       expect(mockUnobserve).toHaveBeenCalledTimes(0)
     })
 
     it("should call unobserve method only on unmount", () => {
       const { unmount } = renderHook(() =>
-        useIntersecting({ ref, hasMore: true }),
+        useIntersecting({ targetRef, hasMore: true }),
       )
       expect(mockUnobserve).toHaveBeenCalledTimes(0)
       unmount()
@@ -51,7 +52,7 @@ describe("useIntersecting", () => {
         }
       })
       const { result } = renderHook(() =>
-        useIntersecting({ ref, hasMore: true }),
+        useIntersecting({ targetRef, hasMore: true }),
       )
       expect(result.current).toBeTruthy()
     })
@@ -65,7 +66,7 @@ describe("useIntersecting", () => {
         }
       })
       const { result } = renderHook(() =>
-        useIntersecting({ ref, hasMore: false }),
+        useIntersecting({ targetRef, hasMore: false }),
       )
       expect(result.current).toBeFalsy()
     })
