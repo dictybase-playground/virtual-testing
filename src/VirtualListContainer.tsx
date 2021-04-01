cf875504c917e0819a212ca4dc207da0b3a906e3
wildlifehexagon <hartline.eric@gmail.com>
2021-03-31 20:36:39 -0500

    refactor: remove commented out code

--- src/VirtualListContainer.tsx
+++ src/VirtualListContainer.tsx
@@ -4,9 +4,6 @@ import VirtualList from "./VirtualList"
 import { GET_STRAIN_LIST } from "./query"
 
 const VirtualListContainer = () => {
-  // const [hasMore, setHasMore] = React.useState(true)
-  // const [isLoadingMore, setIsLoadingMore] = React.useState(false)
-  // const [prevCursor, setPrevCursor] = React.useState(null)
   const { loading, error, data } = useQuery(GET_STRAIN_LIST, {
     variables: {
       cursor: 0,
@@ -18,51 +15,7 @@ const VirtualListContainer = () => {
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error :(</p>
 
-  // const loadMoreItems = () => {
-  //   const newCursor = data.listStrains.nextCursor
-  //   if (newCursor === prevCursor) {
-  //     return
-  //   }
-  //   setPrevCursor(newCursor)
-  //   setIsLoadingMore(true)
-  //   fetchMore({
-  //     query: GET_STRAIN_LIST,
-  //     variables: {
-  //       cursor: newCursor,
-  //       limit: 30000,
-  //       filter: "",
-  //     },
-  //     updateQuery: (previousResult: any, { fetchMoreResult }: any) => {
-  //       setIsLoadingMore(false)
-  //       const previousEntry = previousResult.listStrains
-  //       const previousStrains = previousEntry.strains
-  //       const newStrains = fetchMoreResult.listStrains.strains
-  //       const newCursor = fetchMoreResult.listStrains.nextCursor
-  //       const allStrains = [...previousStrains, ...newStrains]
-
-  //       if (newCursor === 0) {
-  //         setHasMore(false)
-  //       }
-
-  //       return {
-  //         listStrains: {
-  //           nextCursor: newCursor,
-  //           strains: [...new Set(allStrains)],
-  //           __typename: previousEntry.__typename,
-  //         },
-  //       }
-  //     },
-  //   })
-  // }
-
-  return (
-    <VirtualList
-      data={data.listStrains.strains}
-      // loadMore={loadMoreItems}
-      // hasMore={hasMore}
-      // isLoadingMore={isLoadingMore}
-    />
-  )
+  return <VirtualList data={data.listStrains.strains} />
 }
 
 export default VirtualListContainer
