ae59c270a2912d6537acef216d5e9d26b8584852
wildlifehexagon <hartline.eric@gmail.com>
2021-04-05 13:17:04 -0500

    chore: add cross-fetch

--- src/utils/apolloClient.ts
+++ src/utils/apolloClient.ts
@@ -1,5 +1,6 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client"
 import { setContext } from "@apollo/client/link/context"
+import fetch from "cross-fetch"
 
 const client = new ApolloClient({
   link: setContext((request, { headers }) => {
@@ -13,6 +14,7 @@ const client = new ApolloClient({
     createHttpLink({
       uri: "https://ericgraphql.dictybase.dev/graphql",
       credentials: "include",
+      fetch,
     }),
   ),
   cache: new InMemoryCache(),
